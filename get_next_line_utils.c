67d24452e970d4928701aaa8695534d8d9501c9f
user42 <eliarisalvares@gmail.com>
2021-09-05 09:26:36 -0300

    bora

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 10:43:18 by elraira-          #+#    #+#             */
-/*   Updated: 2021/09/05 09:20:26 by elraira-         ###   ########.fr       */
+/*   Updated: 2021/09/05 09:26:30 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,6 @@ char	*ft_strjoin(char *s1, char *s2)
 	while (*s2)
 		*str++ = *s2++;
 	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-	free(s1);
 	return (str);
 }
 
