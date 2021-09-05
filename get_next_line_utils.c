3b09cde9af78ab16fc292c38c1c465133cdf8e62
user42 <eliarisalvares@gmail.com>
2021-09-05 10:23:47 -0300

    bora

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: elraira- <elraira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 10:43:18 by elraira-          #+#    #+#             */
-/*   Updated: 2021/09/05 10:12:47 by elraira-         ###   ########.fr       */
+/*   Updated: 2021/09/05 10:20:36 by elraira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,6 @@ size_t	ft_strlen(char *str)
 	while (str[c] != '\0')
 		c++;
 	return (c);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*str;
-
-	if (!s1)
-		{
-			s1 = (char *)malloc(1 * sizeof(char));
-			s1[0] = '\0';
-		}
-	if (!s1 || !s2)
-		return (NULL);
-	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	if(s1)
-	{
-		while (*s1)
-			*str++ = *s1++;
-	}
-	while (*s2)
-		*str++ = *s2++;
-	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-	free(s1);
-	return (str);
 }
 
 char	*ft_strchr(char *s, int c)
@@ -66,4 +40,35 @@ char	*ft_strchr(char *s, int c)
 		i++;
 	}
 	return (0);
+}
+
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t c;
+	char	*str;
+
+	if (!s1)
+		{
+			s1 = (char *)malloc(1 * sizeof(char));
+			s1[0] = '\0';
+		}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if(s1)
+	{
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	}
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }
